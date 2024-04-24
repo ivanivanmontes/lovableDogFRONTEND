4291ba1abfe7e5cf133ea800a7d9f81d2239c13b
Ivan Montes <101290628+ivanivanmontes@users.noreply.github.com>
2024-04-24 11:30:13 +0200

    upload image???

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,21 +1,48 @@
-import { StatusBar } from "expo-status-bar";
-import { View, Text, StyleSheet} from "react-native";
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button, Image  } from 'react-native';
+import { launchImageLibrary } from 'react-native-image-picker';
+import axios from 'axios';
+import * as ImagePicker from 'expo-image-picker';
+
 
 const SettingsScreen = () => {
-    return (
-      <View style={styles.container}>
-        <Text>Settings Screen</Text>
-      </View>
-    );
-  };
+  const [image, setImage] = useState(null);
+  const handleFileUpload = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    console.log(result)
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button title="Upload File" onPress={handleFileUpload} />
+      {image && <Image source={{ uri: image }} style={styles.image} />}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-container: {
+  container: {
     flex: 1,
     backgroundColor: '#FFE5DC',
     alignItems: 'center',
     justifyContent: 'center',
-},
+  },
+  uploadStatus: {
+    marginTop: 20,
+    color: 'red',
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
 });
 
 export default SettingsScreen;
